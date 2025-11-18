369aaa1bd0cd9f05daeb941a087054458b1375da
Giancarlo Bedoya <gian.05.05@hotmail.com>
2025-11-18 09:47:38 -0500

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { Header } from "./components/header";
 import { About } from "./components/about";
 import JsonData from "./data/data.json";
 import SmoothScroll from "smooth-scroll";
-// import "./App.css";
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
